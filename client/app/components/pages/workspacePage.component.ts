03c0f7bf89e2c35faeb2b6a1acb9f205d7897041
Sahar <sahare92@gmail.com>
2017-08-11 16:10:39 +0300

    annotation loaded

--- client/app/components/pages/workspacePage.component.ts
+++ client/app/components/pages/workspacePage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Page } from '../../models/Page';
 
 @Component({
   moduleId: module.id,
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
 })
 
 export class WorkspacePageComponent {
+	currentPage: Page;
+	// TODO remove this variables just to test
+	annotations: object[];
+	annotationController;
+
+	constructor(){
+		this.init();
+	}
+
+	init() {
+		this.currentPage = new Page(null);
+		this.currentPage.src = "http://i.onionstatic.com/avclub/5667/44/16x9/960.jpg";
+	}
+
+	loadAnnotationsFromDB() {
+		this.annotations = [
+			{
+				/** The URL of the image where the annotation should go **/
+				src : this.currentPage.src,
+				/** The annotation text **/
+				text : "test_text",
+				/** The annotation shape **/
+				shapes : [
+					{
+						type: 'rect',
+						geometry: {
+							x:  0.2,
+							y:  0.3,
+							width:  0.2,
+							height:  0.3
+						}
+					}
+				]
+			}
+		]
+		window.anno.addAnnotation(this.annotations[0], this.annotationController);
+	}
+
+	loadAnnotations() {
+		this.annotationController = window.anno.makeAnnotatable(document.getElementById('anno-img'));
+		this.loadAnnotationsFromDB();
+	}
 }
---
+++ client/app/models/Page.ts
@@ -0,0 +1,13 @@
+export class Page {
+	_id: number;
+	name: string;
+	src: string;
+
+	constructor(data){
+		if(data != null){
+			this._id = data._id;
+			this.name = data.name;
+			this.src = data.src;
+		}
+	}
+}
