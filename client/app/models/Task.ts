befbf60f73f585e4545c935159f62cdd8d8583c0
sahar <sahare92@gmail.com>
2017-11-11 18:48:11 +0200

    added task model in FE

---
+++ client/app/models/Task.ts
@@ -0,0 +1,19 @@
+import { User } from './User';
+
+export class Page {
+	_id: Number;
+	assigner: User;
+	annotator: User;
+	verifier: User;
+	verified: Boolean;
+
+	constructor(data){
+		if(data != null){
+			this._id = data._id;
+			this.assigner = data.assigner;
+			this.annotator = data.annotator;
+			this.verifier = data.verifier;
+			this.verified = data.verified;
+		}
+	}
+}
