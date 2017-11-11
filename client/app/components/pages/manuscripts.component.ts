afe157db4b4056b97205e2e90937ccb0ecab0ec9
sahar <sahare92@gmail.com>
2017-11-11 18:57:56 +0200

    added tasks frontend and manuscript page

--- client/app/components/pages/manuscripts.component.ts
+++ client/app/components/pages/manuscripts.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 import { ManuscriptsService } from '../../services/manuscript.service';
 import { Manuscript } from '../../models/Manuscript';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UsersService } from '../../services/users.service'
+import { TasksService } from '../../services/tasks.service'
+import { Task } from '../../models/Task';
 
 import { Page } from '../../models/Page';
+import { User } from '../../models/User';
 
 
 @Component({
@@ -14,6 +18,7 @@ import { Page } from '../../models/Page';
 })
 
 
+
 export class ManuscriptsComponent {
 	
 	private newMan: Manuscript;
@@ -21,8 +26,19 @@ export class ManuscriptsComponent {
 	private currManuscript: Manuscript;
 	private pageSrc : string;
 	private pageName: String;
+	private currUser :User;
+	private currPages: Page[];
+	private allUsers : User[];
+	private shareableUsers : User[];
+	private selectedUsr :User;
+	private activePage :Page;
+	private worker: User;
+	private roles: string [];
+	private role: string;
+	private canCreateTask: boolean;
+	private tasks: Task[];
 
-	constructor(private mScriptService: ManuscriptsService){
+	constructor(private mScriptService: ManuscriptsService, private uService: UsersService, private tService: TasksService){
 		this.init();
 	}
 
@@ -30,8 +46,122 @@ export class ManuscriptsComponent {
 		this.newMan = new Manuscript(null);
 		this.getExisting();
 		this.currManuscript = null
+		this.getCurrUser(); 
+		this.getAllUsers();
+		this.shareableUsers = [];
+		this.activePage = null;
+		this.worker = null;
+		this.initRoles();
+		this.canCreateTask = false;
+		this.tasks = null;
 		
 	}
+
+	getTasks(){
+		console.log("Getting tasks")
+		this.tService.getTasks( {
+			worker : this.currUser._id,
+			role:'Verifyer',
+			status: "In progress"
+		}).subscribe(
+			r=> {
+				this.tasks = r;
+			},
+			e=>alert("Error loading tasks")
+		)
+	}
+
+	completeTask(t : Task){
+		t.status = "Completed"
+		this.tService.updateTask(t).subscribe(
+			r=>{
+				
+				alert("Task is set as done");
+				this.getTasks();
+			},
+			e => alert(e)
+		)
+		
+	}
+
+	canTaskBeCreated(){
+		 return this.role && this.worker && this.activePage && this.currManuscript 
+	}
+
+	initRoles(){
+		this.roles = ["Annotator", "Verifyer"]
+	}
+
+	selectRole(r){
+		this.role = r;
+	}
+
+	getCurrRole(){
+		if (this.role){
+			return this.role
+		}
+		else{
+			return "Select role"
+		}
+	}
+
+	getCurrPageName(){
+		if (this.activePage){
+			return this.activePage.name
+		}
+		else{
+			return "Please select page"
+		}
+
+	}
+
+	getCurrWorkerName(){
+		if(this.worker){
+			return this.worker.name
+		}
+		else{
+			return "Please select user"
+		}
+
+	}
+
+	getAllUsers(){
+		this.uService.getUsers().subscribe(
+			r => {
+				this.allUsers = r;
+			},
+			e =>{
+				alert("Some error happedened" + e)
+			}
+		)
+	}
+
+	assignTask(){
+		let data = {
+			name:"newT",
+			manuscript : this.currManuscript._id,
+			page : this.activePage._id,
+			role : this.role,
+			worker: this.worker._id,
+			owner: this.currUser._id,
+			note:  "Page: " + this.activePage.name + " In manuscript: " +this.currManuscript.name
+		}
+		let t = new Task(data);
+		this.tService.addTask(t).subscribe(
+			r=>{
+				alert("task created succesfuly")
+				this.getTasks()
+			},
+			err=>{
+				alert("shit happens")
+			}
+		)
+	}
+
+	setWorker(u){
+		this.worker= u;
+	}
+
 	getCurrManuscriptName(){
 		if (this.currManuscript == null){
 			return "Select Manuscript"
@@ -40,9 +170,10 @@ export class ManuscriptsComponent {
 			return this.currManuscript.name;
 		}
 	}
-	getExisting(){
 
+	getExisting(){
 		this.mScriptService.getManuscripts().subscribe(res => {
+			let activeMans;
 			if (res){
 				this.existingManuscript = res;
 			}		
@@ -52,10 +183,73 @@ export class ManuscriptsComponent {
 			});
 		
 	}
+
 	setActiveMan(man: Manuscript){
 		this.currManuscript = man;
+		this.setSharableUsers();
+		this.getTasks();
 	}
+
+	setActivePage(){
+		this.mScriptService.getPages({manuscript: this.currManuscript._id}).subscribe(
+			res =>{
+				this.currPages = res;
+			},
+			err=>{
+				alert(err)
+			}
+		)
+
+	}
+
+	setPage(page){
+		this.activePage = page;
+		
+
+	}
+
+	setSharableUsers(){
+		this.allUsers.forEach(element => {
+			if (element._id != this.currManuscript.owner && 
+				this.currManuscript.shared.indexOf(element._id) == -1){
+				this.shareableUsers.push(element);
+			}
+			this.shareableUsers.forEach(element => {
+
+			});
+		});
+	}
+
+	setActiveManandPages(man: Manuscript){
+		this.setActiveMan(man);
+		this.getPages();
+	}
+
+	selectUsr(usr){
+		this.selectedUsr = usr;
+	}
+
+	restartMans(){
+		this.shareableUsers = []
+		this.setSharableUsers();
+	}
+
+	shareMan(){
+		this.currManuscript.shared.push(this.selectedUsr._id)
+		this.mScriptService.updateMan(this.currManuscript).subscribe(
+			res=>{
+				alert("Manuscript shared succefully!")
+				this.restartMans();
+			},
+			err=>{
+				alert("Something went wrong sharing manuscript")
+			}
+		)
+	}
+
 	createManuscript(){
+		this.newMan.owner = this.currUser._id;
+		
 		this.mScriptService.addManuscript(this.newMan).subscribe(
 			res => {
 				alert("Manuscript created successfully!");
@@ -66,15 +260,50 @@ export class ManuscriptsComponent {
 				alert(err._body);
 			});
 	}
+
 	createPage(){
 		var data = {manuscript: this.currManuscript._id,
 						 name:this.pageName, 
 						 image:this.pageSrc}
 		var p = new Page(data);
 		this.mScriptService.createPage(p).subscribe(res =>{
+			this.getPages();
 			alert("Page was created succesfully")
 		}, err=>{
 			alert("Error creating page!")
 		});
+	}
+
+	getShareableUsers(){
+		if (this.selectedUsr == null){
+			return "Please select user"
+		}
+		else{
+			return this.selectedUsr.name
+		}
+	}
+
+	getCurrUser(){
+		this.uService.getLoggedUser().subscribe(
+			r=>{
+				this.currUser = r;
+			}
+			,
+			s=>{
+				alert("NO LOGGED USER!!@##%%@$")
+			}
+			)
+	}
+
+	getPages(){
+		let query = {manuscript : this.currManuscript._id};
+		this.mScriptService.getPages(query).subscribe(
+			r => {
+				this.currPages = r;
+			},
+			e => {
+				alert(e + "something went wrong getting pages")
+			}
+		)
 	}
 }
---
+++ client/app/services/tasks.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class TasksService {
+    constructor(private http:Http){}
+    getTasks(query){
+		var url = '/api/tasks?';
+		console.log(query)
+		for(var p in query){
+			if(query.hasOwnProperty(p)){
+				url = url.concat(p + '=' + query[p]);
+				url = url.concat("&")
+			}
+		}
+		return this.http.get(url)
+			.map(res => {
+				if (res.status < 200 || res.status >= 300)
+					throw new Error();
+				else
+					return res.json();
+		});
+    }
+
+    addTask(t) {
+		var headers = new Headers();
+		console.log(t)
+		headers.append('Content-Type', 'application/json');
+		return this.http.post('/api/tasks', JSON.stringify(t), {headers: headers})
+			.map(res => {
+				if (res.status < 200 || res.status >= 300)
+					throw new Error();
+				else
+					return res.json(); 
+		});
+	}
+	updateTask(t){
+		var headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+		return this.http.put('/api/tasks/'+t._id, JSON.stringify(t), {headers: headers})
+			.map(res => {
+				if (res.status < 200 || res.status >= 300)
+					throw new Error();
+				else
+					return res.json();
+		});
+	}
+}
